85f64a2902dbe86e931aef817e63944309255bec
Yagudagu <Jacobhmartian@gmail.com>
2021-05-06 22:04:34 +0000

    changed port stuff

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.get('/crash', (next) => {
     next();
 });
 
-const port = 80;
+const port = process.env.PORT || 3000
+
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
 })
